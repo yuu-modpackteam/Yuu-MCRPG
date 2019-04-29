3e387669daf2f78774f20d0219df94d978bb3227
yuu_111 <yuu210yuu@gmail.com>
2019-04-29 12:03:52 +0900

    kotlin

---
+++ src/main/java/yuu/rpg/RPG.kt
@@ -0,0 +1,15 @@
+package yuu.rpg
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class RPG : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
